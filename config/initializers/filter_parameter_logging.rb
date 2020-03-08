9579dff18d0e7f74a9f22cd65fd73abb9e5dd4ce
Etienne Depaulis <etienne.depaulis@lifen.fr>
2020-03-08 11:21:40 +0100

    Avoid loggin sensitive attributes

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :password_confirmation, :first_name, :last_name, :cellphone_number, :otp_secret, :email, :public_token]
